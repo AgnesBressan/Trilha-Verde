d1ffa7c9da21843773480bfde1a2f02eedb9fcb6
RauanySecci <rauanymartinez@usp.br>
2025-06-12 09:15:44 -0300

    adicionando o back2

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class TrilhaVerdeApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => const TelaQuiz(),
+        '/': (context) => const TelaInicial(),
         '/login': (context) => TelaLogin(),
         '/principal': (context) => const TelaPrincipal(),
         '/menu': (context) => const TelaMenu(),
